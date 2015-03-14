cb4e1d524ab37a1dc8197115b2a1807fff334129
Michał Cywiński <mcywinski@hotmail.com>
2015-03-14 22:23:16 +0100

    Initial API methods for fetching lists.

--- FishMash/app/controllers/Api/lists_controller.rb
+++ FishMash/app/controllers/Api/lists_controller.rb
@@ -2,8 +2,14 @@ class Api::ListsController < ApplicationController
   respond_to :json, :xml
 
   def index
+    lists = WordList.all
+    respond_with lists
   end
 
   def show
+    list = WordList.find params[:id]
+    respond_with list
   end
+
+  private
 end
