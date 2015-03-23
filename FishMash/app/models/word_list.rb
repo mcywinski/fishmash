67049eb45cced92969154edd4d86b65c03ecf399
Michał Cywiński <mcywinski@hotmail.com>
2015-03-23 22:03:27 +0100

    Typo logical fix for active record associations.

--- FishMash/app/models/word_list.rb
+++ FishMash/app/models/word_list.rb
@@ -1,6 +1,6 @@
 class WordList < ActiveRecord::Base
   belongs_to :main_language, class_name: "Language", foreign_key: "main_language_id"
-  belongs_to :foreign_language, class_name: "WordList", foreign_key: "foreign_language_id"
+  belongs_to :foreign_language, class_name: "Language", foreign_key: "foreign_language_id"
 
   has_many :word_list_memberships, dependent: :destroy
   has_many :words, through: :word_list_memberships
