3519538135b8d1dcc313940219f3db41e9c8a3ee
Konrad Zdrojewski <konrad.zdrojewski@wat.edu.pl>
2015-03-24 23:11:48 +0100

    LearningActivity is showing phrases along with meaning, and that makes learning feature fully working.

--- app/src/main/java/net/elenx/fishmash/activities/LearningActivity.java
+++ app/src/main/java/net/elenx/fishmash/activities/LearningActivity.java
@@ -1,14 +1,18 @@
 package net.elenx.fishmash.activities;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import net.elenx.fishmash.R;
+import net.elenx.fishmash.daos.WordsDAO;
+import net.elenx.fishmash.models.Word;
+
+import java.util.Iterator;
 
 public class LearningActivity extends OptionsActivity
 {
-    private Button nextWordButton;
-
     @Override
     protected void onPostCreate(Bundle savedInstanceState)
     {
@@ -17,6 +21,40 @@ public class LearningActivity extends OptionsActivity
 
         updateWords(getIntent().getLongExtra("wordListId", -1));
 
-        nextWordButton = (Button) findViewById(R.id.nextWordButton);
+        prepareForLearning();
+    }
+
+    private void prepareForLearning()
+    {
+        final TextView phraseTextView = (TextView) findViewById(R.id.phraseTextView);
+        final TextView meaningTextView = (TextView) findViewById(R.id.meaningTextView);
+
+        final Iterator<Word> wordIterator = new WordsDAO(this).selectAll().iterator();
+
+        Button nextWordButton = (Button) findViewById(R.id.nextWordButton);
+        nextWordButton.setOnClickListener
+        (
+            new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    if(wordIterator.hasNext())
+                    {
+                        Word word = wordIterator.next();
+
+                        phraseTextView.setText(word.getPhrase());
+                        meaningTextView.setText(word.getMeaning());
+                    }
+                    else
+                    {
+                        mainMenu();
+                    }
+                }
+            }
+        );
+
+        // show first word
+        nextWordButton.performClick();
     }
 }
