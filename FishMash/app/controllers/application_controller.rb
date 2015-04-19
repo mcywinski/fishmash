9989e018d263fd1a86f7eb30cd193d5d3ac4e86a
Michał Cywiński <mcywinski@hotmail.com>
2015-04-19 22:16:42 +0200

    Logowanie/wylogowanie użytkownika.

--- FishMash/app/controllers/application_controller.rb
+++ FishMash/app/controllers/application_controller.rb
@@ -2,8 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :is_user_logged_in?
 
   def api
+  end
+
+  def is_user_logged_in?
+    !(session[:logged_user_id].nil?)
+  end
+
+  def set_logged_user_id(id)
+    session[:logged_user_id] = id
+  end
+
+  def get_logged_user_id
+    session[:logged_user_id]
   end
 
   def stringify_errors(model)
--- FishMash/app/controllers/users_controller.rb
+++ FishMash/app/controllers/users_controller.rb
@@ -18,9 +18,31 @@ class UsersController < ApplicationController
   def login
   end
 
+  def authenticate
+    user = User.find_by(login: user_login_params[:login]).try(:authenticate, user_login_params[:password])
+    if !user
+      @errors = 'Invalid login or password provided'
+      render 'login'
+    else
+      set_logged_user_id(user.id)
+      flash[:success] = "You've successfuly signed in."
+      redirect_to root_path
+    end
+  end
+
+  def logout
+    session.destroy
+    flash[:success] = 'You are successfuly logged out.'
+    redirect_to root_path
+  end
+
   private
 
     def user_create_params
       params.require(:user).permit(:login, :email, :password, :password_confirmation)
+    end
+
+    def user_login_params
+      params.require(:user).permit(:login, :password)
     end
 end
