81e88ffc153d66bc335644472d5cedd652bd1df4
Michał Cywiński <mcywinski@hotmail.com>
2015-04-22 11:08:23 +0200

    Validation messages fixes for custom language difference validator

--- FishMash/app/validators/language_difference_validator.rb
+++ FishMash/app/validators/language_difference_validator.rb
@@ -2,13 +2,13 @@ class LanguageDifferenceValidator < ActiveModel::Validator
   include ApplicationHelper
 
   def validate(record)
-    message = 'A language must be selected.'
+    message = 'must be selected.'
     if record.main_language_id == DEFAULT_OPTION_ID or record.foreign_language_id == DEFAULT_OPTION_ID
       record.errors[:foreign_language_id] << message
       puts "#{record.class.name}:#{message}"
     end
 
-    message = 'Languages must be different.'
+    message = 'must be different.'
     if record.main_language_id == record.foreign_language_id
       record.errors[:foreign_language_id] << message
       puts "#{record.class.name}:#{message}"
