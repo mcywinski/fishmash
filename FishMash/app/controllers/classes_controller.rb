74be277ce81e4fb2e0822d4986867ee08fd79ae0
Michał Cywiński <mcywinski@hotmail.com>
2015-05-17 23:45:36 +0200

    Edycja podstawowych danych klasy

--- FishMash/app/controllers/classes_controller.rb
+++ FishMash/app/controllers/classes_controller.rb
@@ -12,17 +12,32 @@ class ClassesController < ApplicationController
   end
 
   def create
-    stud_class = StudentClass.new(class_create_params)
+    stud_class = StudentClass.new(class_params)
     if stud_class.save
       redirect_to classes_path
     else
       flash[:errors] = stringify_errors(stud_class)
-      render 'new'
+      redirect_to new_class_path and return
     end
   end
 
+  def edit
+    @student_class = StudentClass.find(params[:id])
+  end
+
+  def update
+    @student_class = StudentClass.find(params[:id])
+    if !@student_class.update(class_params)
+      flash[:errors] = stringify_errors(@student_class)
+      redirect_to edit_class_path(@student_class) and return
+    end
+
+    render 'edit'
+
+  end
+
   private
-    def class_create_params
+    def class_params
       params.require(:student_class).permit(:description, :name)
     end
 end
