4ffb1e18f2058d55c742267e70cc0c4070d01119
Michał Cywiński <mcywinski@hotmail.com>
2015-05-17 22:41:41 +0200

    Creating new base objects for classes

--- FishMash/app/controllers/classes_controller.rb
+++ FishMash/app/controllers/classes_controller.rb
@@ -1,7 +1,28 @@
 class ClassesController < ApplicationController
+  before_action :require_login
+
   # Displays all classes in the system
   # TODO: Display only classes belonging to a user
   def index
 
   end
+
+  def new
+
+  end
+
+  def create
+    stud_class = StudentClass.new(class_create_params)
+    if stud_class.save
+      redirect_to classes_path
+    else
+      flash[:errors] = stringify_errors(stud_class)
+      render 'new'
+    end
+  end
+
+  private
+    def class_create_params
+      params.require(:student_class).permit(:description, :name)
+    end
 end
