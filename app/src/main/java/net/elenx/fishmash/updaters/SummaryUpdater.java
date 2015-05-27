d5b7bc8d13649dfee88e3dedf74a5b5fc254b0ce
Konrad Zdrojewski <konrad.zdrojewski@wat.edu.pl>
2015-05-27 02:59:56 +0200

    SummaryUpdater has been built.

---
+++ app/src/main/java/net/elenx/fishmash/updaters/SummaryUpdater.java
@@ -0,0 +1,27 @@
+package net.elenx.fishmash.updaters;
+
+import net.elenx.fishmash.Constant;
+import net.elenx.fishmash.activities.core.OptionsActivity;
+import net.elenx.fishmash.models.ExamSummary;
+
+import java.util.Map;
+
+public class SummaryUpdater extends FishmashUpdater
+{
+    private long examId;
+
+    public SummaryUpdater(OptionsActivity optionsActivity, long examId)
+    {
+        super(optionsActivity);
+        this.examId = examId;
+    }
+
+    @Override
+    void download() throws Exception
+    {
+        Map<String, String> parameters = buildParameters();
+        parameters.put("exam_id", String.valueOf(examId));
+
+        ExamSummary[] examSummaries = fishmashRest.getForObject(Constant.SUMMARY_EXAMID_TOKEN, ExamSummary[].class, parameters);
+    }
+}
