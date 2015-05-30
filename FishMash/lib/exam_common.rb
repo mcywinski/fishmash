c9cb2014329cf52dd5f6b0bd4bcc960c5039de62
Michał Cywiński <mcywinski@hotmail.com>
2015-05-30 12:27:18 +0200

    Exam start dates included in views.

--- FishMash/lib/exam_common.rb
+++ FishMash/lib/exam_common.rb
@@ -1,12 +1,12 @@
 module ExamCommon
   def ExamCommon.is_start_overdue?(id)
     exam = get_exam(id)
-    return exam.date_exam_finish < Date.today
+    return (exam.date_exam_finish < Date.today or exam.date_exam_start > Date.today)
   end
 
   def ExamCommon.is_practice_overdue?(id)
     exam = get_exam(id)
-    return exam.date_practice_finish < Date.today
+    return (exam.date_practice_finish < Date.today or exam.date_practice_start > Date.today)
   end
 
   def ExamCommon.get_exam(id)
