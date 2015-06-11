472cd3bc4fc46ae1993de6d91a06488f905f5e36
Michał Cywiński <mcywinski@hotmail.com>
2015-06-11 23:10:49 +0200

    Dodana aplikacja iOS.

---
+++ iOSFishMash/iOSFishMashTests/iOSFishMashTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSFishMashTests.swift
+//  iOSFishMashTests
+//
+//  Created by Michał Cywiński on 11.06.2015.
+//  Copyright (c) 2015 Ten Guys One Code. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iOSFishMashTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
