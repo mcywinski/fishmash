18b4138f871ec0f04ea59dcc4c8487e9106ed43b
qliss <qliss@o2.pl>
2015-06-16 12:47:24 +0200

    no message

---
+++ FishMash-WP8/FishMashNew/Models/ChangePasswordModels/PasswordChangeResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishMashNew.Models.ChangePasswordModels
+{
+    public class PasswordChangeResult
+    {
+        public bool success { get; set; }
+        public int result_status { get; set; }
+    }
+}
